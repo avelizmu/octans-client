c895de3c42f13c0863a85e9f044a62af81e3e027
Adrian Veliz <adrian.veliz@ecoprintq.com>
2022-01-09 00:09:29 -0500

    Add tag and media search in home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,10 +1,60 @@
-import {useState, useContext} from 'react';
-import {Navigate, Outlet, useParams} from "react-router-dom";
+import {useState, useEffect} from 'react';
+import {Navigate} from "react-router-dom";
 import styles from "./HomePage.module.css";
+import axios from "axios";
+
+type Media =  {
+    id: number,
+    hash: string,
+    mediaType: string,
+    width: number,
+    height: number,
+    duration: number,
+    size: number,
+    created: Date,
+    createdBy: number
+}
+
+type Tag = {
+    id: number,
+    type: string,
+    namespace: string,
+    tagName: string
+}
 
 export default function HomePage() {
     const [selectedTab, setSelectedTab] = useState(0);
     const tabs = ['Your Media', 'Shared With You', 'All Media', 'Upload'];
+    const [media, setMedia] = useState<Media[]>([]);
+    const [tags, setTags] = useState<Tag[]>([]);
+    const [suggestedTags, setSuggestedTags] = useState<Tag[]>([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        if(search === '') {
+            setSuggestedTags([]);
+            return;
+        }
+        let queryString = `/api/tags/search?search=${search}`;
+        if(tags.length) {
+            queryString = `${queryString}&exclude[]=${tags.map(t => t.id).join('&exclude[]=')}`
+        }
+        axios.get(queryString)
+            .then(({data}) => {
+                setSuggestedTags(data);
+            })
+    }, [search])
+
+    useEffect(() => {
+        let queryString = '/api/media/list?type=Self';
+        if(tags.length) {
+            queryString = `${queryString}&tags[]=${tags.map(t => t.id).join('&tags[]=')}`
+        }
+        axios.get(queryString)
+            .then(({data}) => {
+                setMedia(data);
+            })
+    }, [tags])
 
     if(tabs[selectedTab] === 'Upload') {
         return <Navigate to={'/upload'}/>
@@ -24,10 +74,68 @@ export default function HomePage() {
                 )
             }
         </div>
-        <input className={styles.searchBar}/>
+        <div className={styles.tags}>
+            {
+                tags.map(tag =>
+                    <div className={styles.tag}
+                         onClick={() => {
+
+                         }}
+                    >
+                        <div className={styles.tagContent}>
+                            <div>
+                                {
+                                    tag.type === 'SYSTEM' ? 'System Tag' : ''
+                                }
+                            </div>
+                            <div>
+                                {tag.namespace}:{tag.tagName}
+                            </div>
+                        </div>
+                        <button className={styles.deleteButton} onClick={() => {
+                            setTags([...tags.filter(t => t.id !== tag.id)])
+                        }}>X</button>
+
+                    </div>
+                )
+            }
+        </div>
+        <input value={search} className={styles.searchBar} onChange={(e => setSearch(e.target.value))} onKeyUp={(e) => {
+            if(e.key === 'Enter') {
+                if(search.includes(':') && search.split(':')[1].length) {
+                    axios.post('/api/tags', {
+                        namespace: search.split(':')[0],
+                        tagName: search.split(':')[1]
+                    }).then(({data}) => {
+                        setTags([...tags, data]);
+                        setSearch('')
+                    });
+                }
+            }
+        }}/>
+        <div className={styles.tagSuggestions}>
+            {
+                suggestedTags.map(tag =>
+                        <div className={styles.tagSuggestion}
+                             onClick={() => {
+                                 setTags([...tags, tag]);
+                                 setSearch('');
+                             }}
+                        >
+                            {tag.type === 'SYSTEM' ? 'System Tag: ' : ''} {tag.namespace}:{tag.tagName}
+                        </div>
+                )
+            }
+        </div>
         <div className={styles.searchResults}>
             {
-                // TODO Implement search
+                media.map(media =>
+                        <div>
+                            {
+                                media.id
+                            } |
+                        </div>
+                )
             }
         </div>
     </div>
